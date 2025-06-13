a36b652d787c6a838b6a21abee50f48cb15982a2
Jason Pruitt <55477390+Json-To-String@users.noreply.github.com>
2025-06-13 11:05:17 -0400

    fixing docs in readme, adding dynamic versioning

--- src/naqs_devices/PulseBlasterESRPro300/__init__.py
+++ src/naqs_devices/PulseBlasterESRPro300/__init__.py
@@ -10,3 +10,5 @@ from labscript_devices import deprecated_import_alias
 # For backwards compatibility with old experiment scripts:
 PulseBlasterESRPro300 = deprecated_import_alias(
     "naqs_devices.PulseBlasterESRPro300.labscript_devices.PulseBlasterESRPro300")
+
+__version__ = '0.1.0'
